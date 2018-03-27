30a8cb6a75e01df610ffbe46122472eba5e81c9d
NagRock <kuster.maciej@gmail.com>
2018-03-27 15:17:29 +0200

    Sample code showing ngx-auto-scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IntervalObservable } from "rxjs/observable/IntervalObservable";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  items = [];
+  currentItem = 0;
+
+  constructor() {
+    IntervalObservable.create(200).subscribe(() => {
+      this.items.push(`Sample text: ${this.currentItem++}`)
+    });
+  }
 }
